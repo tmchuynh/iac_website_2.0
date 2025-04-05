e2e110677b9b010ca0910158f7462c1a3e161912
Tina Huynh <tinamchuynh@gmail.com>
2025-04-04 23:16:03 -0700

    Add feature and program constants for educational offerings

---
+++ src/lib/constants/list.tsx
@@ -0,0 +1,146 @@
+import { FaBrain, FaChartPie, FaGraduationCap, FaMedal } from "react-icons/fa";
+
+export const features = [
+  {
+    icon: FaMedal,
+    bgClass: "bg-accent-1",
+    text: "Trusted by leading schools and parents",
+  },
+  {
+    icon: FaGraduationCap,
+    bgClass: "bg-accent-2",
+    text: "Experienced & Dedicated Professional Instructors",
+  },
+  {
+    icon: FaChartPie,
+    bgClass: "bg-accent-2",
+    text: "Well diversed classes in various disciplines",
+  },
+  {
+    icon: FaBrain,
+    bgClass: "bg-accent-2",
+    text: "Inspires growth and collaboration",
+  },
+];
+
+export const commitments = [
+  "Universal Access: We ensure that all students have equal access to every program and resource we offer.",
+  "Safe and Respectful Environments: We create spaces where students can collaborate, learn, and express themselves without fear of discrimination or judgment.",
+  "Continuous Improvement: We are dedicated to actively identifying and removing barriers to inclusion, ensuring that every student feels welcomed and valued.",
+];
+
+export const success_plan = [
+  {
+    title: "Comprehensive Success:",
+    description:
+      "Our Comprehensive Success plan is designed as a long-term strategy, not a short-term fix. We offer a multi-year pathway that empowers students with skills for lifelong learning and growth, ensuring they thrive academically and personally, long after the program ends.",
+  },
+  {
+    title: "Advanced Resources:",
+    description:
+      "We are committed to providing advanced resources that keep students at the forefront of STEM education. With the latest technology and materials, our resources are continuously updated to align with the evolving educational landscape, helping students stay ahead of the curve.",
+  },
+  {
+    title: "Refined Curriculum:",
+    description:
+      "The core of our program is a refined curriculum that is continuously improved using data-driven insights and educational research. This ensures that our content remains relevant, fresh, and aligned with the latest standards, offering the most effective educational experience for your students.",
+  },
+];
+
+export const arc_program = [
+  {
+    title: "Immersion:",
+    description:
+      "The immersive nature of the program ensures that students are not just passive learners, but active participants in their education. By engaging students through hands-on experiences, real-world applications, and interactive learning opportunities, we spark their natural curiosity and ignite their creativity. This approach fosters a deeper understanding of the subject matter, encouraging students to make meaningful connections between what they learn in the classroom and the world around them. Immersion promotes critical thinking, problem-solving, and innovation, equipping students with the skills they need to tackle real-life challenges with confidence.",
+  },
+  {
+    title: "Effectiveness:",
+    description:
+      "At the heart of the A.R.C. for Education program is its unwavering focus on effectiveness. Every lesson, resource, and activity is carefully designed to provide measurable results and deliver lasting educational value. We emphasize not only academic success but also the development of essential life skills—such as communication, teamwork, and leadership—that students can apply throughout their personal and professional lives. Our curriculum is continuously refined and enhanced through feedback and data-driven insights to ensure it remains relevant and impactful, giving students the best possible learning experience.",
+  },
+  {
+    title: "Inclusivity:",
+    description:
+      "Inclusivity is a core value that shapes every aspect of our program. We believe that every student—regardless of their abilities, background, or learning style—deserves equal opportunities to thrive. Our approach is highly adaptable, with personalized learning pathways that accommodate the diverse needs of all learners. Whether students are just beginning their educational journey or are already advanced in their studies, we offer tailored support to ensure that each student feels valued and empowered. We create an environment where differences are celebrated, and each student is encouraged to contribute their unique perspective, fostering a sense of belonging and community.",
+  },
+];
+
+export const class_breakdown = [
+  {
+    title: "Attendance Verification:",
+    description:
+      "Before delving into the day's lessons, instructors begin each class by verifying attendance. They cross-reference the list of enrolled students with records from the front office and parents to ensure that every student is accounted for. This initial step helps maintain a secure and structured learning environment.",
+  },
+  {
+    title: "Introduction of New Concepts:",
+    description:
+      "As the class progresses, students eagerly delve into the exploration of new concepts. Instructors introduce fresh subject matter, building upon the foundation laid in previous sessions. This phase serves to expand students' knowledge, stimulate their curiosity, and broaden their understanding of the topic at hand.",
+  },
+  {
+    title: "Practical Application of Learned Concepts:",
+    description:
+      "Students are given the opportunity to put into practice the concepts they have previously learned. This hands-on segment is crucial for reinforcing their understanding and skills. It allows students to engage with the material, collaborate with their peers, and apply their knowledge in a safe and supportive setting.",
+  },
+  {
+    title: "Collaborative Learning:",
+    description:
+      "Our classes also emphasize the importance of teamwork and peer-to-peer interaction. Students are encouraged to engage in discussions, group activities, and collaborative projects. This fosters a sense of community within the class, promotes knowledge sharing, and enhances social and problem-solving skills.",
+  },
+  {
+    title: "Review and Assessment:",
+    description:
+      "Periodically, students engage in review and assessment sessions, where they consolidate their learning by revisiting key concepts and topics. This process helps instructors gauge the progress of each student and identify areas that may require additional support.",
+  },
+  {
+    title: "Q&A and Discussion:",
+    description:
+      "Open dialogue is highly encouraged, and students are invited to ask questions, seek clarification, and engage in meaningful discussions. This interactive approach ensures that every student has the opportunity to actively participate and gain a comprehensive understanding of the subject matter.",
+  },
+];
+
+export const credit_card_procedure = [
+  "When signing your child up for their desired class, indicate if paying by check or credit card.",
+  "If paying by check mail check to the address provided on the flyer.",
+  "If paying by credit card you will receive an invoice from IAC to the email address used in the sign-up form.",
+  "If preferable, you can still call into the office to make a payment with credit card.",
+  "You will be able to pay through Square (via a link in the email invoice) with any major credit card of your choice. You will receive a receipt in your email upon payment.",
+];
+
+export const logos = [
+  {
+    alt: "Arroyo",
+    src: "/images/School_Icons/arroyo.png",
+  },
+  {
+    alt: "Fairmont",
+    src: "/images/School_Icons/fairmont.png",
+  },
+  {
+    alt: "Hicks Elementary",
+    src: "/images/School_Icons/hicks.png",
+  },
+  {
+    alt: "Ladera",
+    src: "/images/School_Icons/ladera.png",
+  },
+  {
+    alt: "Myford",
+    src: "/images/School_Icons/myford.png",
+  },
+  {
+    alt: "Orchard Hillds",
+    src: "/images/School_Icons/orchardhills.png",
+  },
+  {
+    alt: "Peters Canyon",
+    src: "/images/School_Icons/peterscanyon.png",
+  },
+  {
+    alt: "Red Hills",
+    src: "/images/School_Icons/redhill.png",
+  },
+  {
+    alt: "Tustin Ranch",
+    src: "/images/School_Icons/tustinranch.png",
+  },
+];
