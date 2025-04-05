180816e0daaace5f114e0201981f07661ce8159e
Tina Huynh <tinamchuynh@gmail.com>
2025-04-05 01:30:00 -0700

    Add remote image patterns and SVG support in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
+const imageHosts = [
+  "tailwindcss.com",
+  "images.unsplash.com",
+  "plus.unsplash.com",
+  "media.istockphoto.com",
+  "www.shutterstock.com",
+  "cdn.sanity.io",
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+  images: {
+    remotePatterns: imageHosts.map((hostname) => ({
+      protocol: "https",
+      hostname,
+      port: "",
+      pathname: "/**",
+    })),
+    dangerouslyAllowSVG: true,
+  },
 };
 
 export default nextConfig;
