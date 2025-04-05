ddb59cf7a2b282ad1decf80630f9e96c9a514d92
Tina Huynh <tinamchuynh@gmail.com>
2025-04-05 01:31:28 -0700

    Refactor ContactUs component for improved structure and styling, enhancing layout and class organization

--- src/app/contact_us/page.tsx
+++ src/app/contact_us/page.tsx
@@ -1,9 +1,9 @@
 export default function ContactUs() {
   return (
-    <div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div className="relative bg-white px-6 lg:px-8 py-24 sm:py-32 isolate">
       <svg
         aria-hidden="true"
-        className="absolute inset-0 -z-10 size-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+        className="-z-10 absolute inset-0 size-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
       >
         <defs>
           <pattern
@@ -31,19 +31,19 @@ export default function ContactUs() {
         />
       </svg>
       <div className="mx-auto max-w-xl lg:max-w-4xl">
-        <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+        <h2 className="font-semibold text-4xl text-gray-900 text-pretty sm:text-5xl tracking-tight">
           Let’s talk about your project
         </h2>
-        <p className="mt-2 text-lg/8 text-gray-600">
+        <p className="mt-2 text-gray-600 text-lg/8">
           We help companies and individuals build out their brand guidelines.
         </p>
-        <div className="mt-16 flex flex-col gap-16 sm:gap-y-20 lg:flex-row">
+        <div className="flex lg:flex-row flex-col gap-16 sm:gap-y-20 mt-16">
           <form action="#" method="POST" className="lg:flex-auto">
-            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div className="gap-x-8 gap-y-6 grid grid-cols-1 sm:grid-cols-2">
               <div>
                 <label
                   htmlFor="first-name"
-                  className="block text-sm/6 font-semibold text-gray-900"
+                  className="block font-semibold text-gray-900 text-sm/6"
                 >
                   First name
                 </label>
@@ -53,14 +53,14 @@ export default function ContactUs() {
                     name="first-name"
                     type="text"
                     autoComplete="given-name"
-                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                    className="block bg-white px-3.5 py-2 rounded-md w-full text-base text-gray-900 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                   />
                 </div>
               </div>
               <div>
                 <label
                   htmlFor="last-name"
-                  className="block text-sm/6 font-semibold text-gray-900"
+                  className="block font-semibold text-gray-900 text-sm/6"
                 >
                   Last name
                 </label>
@@ -70,14 +70,14 @@ export default function ContactUs() {
                     name="last-name"
                     type="text"
                     autoComplete="family-name"
-                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                    className="block bg-white px-3.5 py-2 rounded-md w-full text-base text-gray-900 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                   />
                 </div>
               </div>
               <div>
                 <label
                   htmlFor="budget"
-                  className="block text-sm/6 font-semibold text-gray-900"
+                  className="block font-semibold text-gray-900 text-sm/6"
                 >
                   Budget
                 </label>
@@ -86,14 +86,14 @@ export default function ContactUs() {
                     id="budget"
                     name="budget"
                     type="text"
-                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                    className="block bg-white px-3.5 py-2 rounded-md w-full text-base text-gray-900 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                   />
                 </div>
               </div>
               <div>
                 <label
                   htmlFor="website"
-                  className="block text-sm/6 font-semibold text-gray-900"
+                  className="block font-semibold text-gray-900 text-sm/6"
                 >
                   Website
                 </label>
@@ -102,14 +102,14 @@ export default function ContactUs() {
                     id="website"
                     name="website"
                     type="url"
-                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                    className="block bg-white px-3.5 py-2 rounded-md w-full text-base text-gray-900 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                   />
                 </div>
               </div>
               <div className="sm:col-span-2">
                 <label
                   htmlFor="message"
-                  className="block text-sm/6 font-semibold text-gray-900"
+                  className="block font-semibold text-gray-900 text-sm/6"
                 >
                   Message
                 </label>
@@ -118,7 +118,7 @@ export default function ContactUs() {
                     id="message"
                     name="message"
                     rows={4}
-                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                    className="block bg-white px-3.5 py-2 rounded-md w-full text-base text-gray-900 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                     defaultValue={""}
                   />
                 </div>
@@ -127,12 +127,12 @@ export default function ContactUs() {
             <div className="mt-10">
               <button
                 type="submit"
-                className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="block bg-indigo-600 hover:bg-indigo-500 shadow-xs px-3.5 py-2.5 rounded-md w-full font-semibold text-center text-sm text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Let’s talk
               </button>
             </div>
-            <p className="mt-4 text-sm/6 text-gray-500">
+            <p className="mt-4 text-gray-500 text-sm/6">
               By submitting this form, I agree to the{" "}
               <a href="#" className="font-semibold text-indigo-600">
                 privacy&nbsp;policy
@@ -140,31 +140,31 @@ export default function ContactUs() {
               .
             </p>
           </form>
-          <div className="lg:mt-6 lg:w-80 lg:flex-none">
+          <div className="lg:flex-none lg:mt-6 lg:w-80">
             <img
               alt=""
               src="https://tailwindcss.com/plus-assets/img/logos/workcation-logo-indigo-600.svg"
-              className="h-12 w-auto"
+              className="w-auto h-12"
             />
             <figure className="mt-10">
-              <blockquote className="text-lg/8 font-semibold text-gray-900">
+              <blockquote className="font-semibold text-gray-900 text-lg/8">
                 <p>
                   “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
                   expedita voluptas culpa sapiente alias molestiae. Numquam
                   corrupti in laborum sed rerum et corporis.”
                 </p>
               </blockquote>
-              <figcaption className="mt-10 flex gap-x-6">
+              <figcaption className="flex gap-x-6 mt-10">
                 <img
                   alt=""
                   src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=96&h=96&q=80"
-                  className="size-12 flex-none rounded-full bg-gray-50"
+                  className="flex-none bg-gray-50 rounded-full size-12"
                 />
                 <div>
-                  <div className="text-base font-semibold text-gray-900">
+                  <div className="font-semibold text-base text-gray-900">
                     Brenna Goyette
                   </div>
-                  <div className="text-sm/6 text-gray-600">
+                  <div className="text-gray-600 text-sm/6">
                     CEO of Workcation
                   </div>
                 </div>
