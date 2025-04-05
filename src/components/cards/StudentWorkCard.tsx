9af4fb9697b429e54221aed55083bcb55aeac9f8
Tina Huynh <tinamchuynh@gmail.com>
2025-04-05 13:55:29 -0700

    Refactor StudentWorkCard component for improved layout and image handling

--- src/components/cards/StudentWorkCard.tsx
+++ src/components/cards/StudentWorkCard.tsx
@@ -1,37 +1,40 @@
 import { StudentProject } from "@/lib/types/cards";
 import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import Image from "next/image";
 
 export default function StudentWorkCard(work: StudentProject, index: number) {
   return (
     <Card key={index} className={index % 2 === 0 ? "border-r-8" : "border-l-8"}>
       <CardHeader className="pb-3">
-        <h4 className="text-lg font-[CuteRabbit] tracking-widest">
+        <h4 className="font-[CuteRabbit] text-lg tracking-widest">
           {work.title}
         </h4>
-        <p className="meta text-xs">
+        <p className="text-xs meta">
           <span className="font-bold">By: </span>
           {work.author}
         </p>
-        <p className="meta text-xs">
+        <p className="text-xs meta">
           <span className="font-bold">Date: </span>
           {work.date}
         </p>
       </CardHeader>
-      <CardContent className="gap-3 flex flex-col">
+      <CardContent className="flex flex-col gap-3">
         {work.description}
         <hr />
-        {work.preview && <p className="italic text-sm">{work.preview}...</p>}
+        {work.preview && <p className="text-sm italic">{work.preview}...</p>}
 
         {work.images && (
           <>
             <hr />
-            <div className="grid grid-cols-3 gap-4">
+            <div className="gap-4 grid grid-cols-3">
               {work.images.map((image, index) => (
-                <img
+                <Image
                   key={index}
                   src={image}
                   alt={`${work.title} image ${index + 1}`}
-                  className="rounded-lg border border-input"
+                  width={300}
+                  height={200}
+                  className="border border-input rounded-lg"
                 />
               ))}
             </div>
