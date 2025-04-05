04233f16286418dea3be28c6c94ada6106de35d3
Tina Huynh <tinamchuynh@gmail.com>
2025-04-05 14:47:13 -0700

    Refactor RequestUs component for improved layout consistency and readability

--- src/app/contact_us/request_us/page.tsx
+++ src/app/contact_us/request_us/page.tsx
@@ -3,29 +3,29 @@ import { FaBuilding, FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 export default function RequestUs() {
   return (
     <div className="relative bg-white">
-      <div className="lg:absolute lg:inset-0 lg:left-1/2">
+      <div className="lg:left-1/2 lg:absolute lg:inset-0">
         <img
           alt=""
           src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=2560&h=3413&&q=80"
-          className="h-64 w-full bg-gray-50 object-cover sm:h-80 lg:absolute lg:h-full"
+          className="lg:absolute bg-gray-50 w-full h-64 sm:h-80 lg:h-full object-cover"
         />
       </div>
-      <div className="pt-16 pb-24 sm:pt-24 sm:pb-32 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:pt-32">
+      <div className="lg:grid lg:grid-cols-2 lg:mx-auto pt-16 sm:pt-24 lg:pt-32 pb-24 sm:pb-32 lg:max-w-7xl">
         <div className="px-6 lg:px-8">
-          <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-            <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+          <div className="mx-auto lg:mx-0 max-w-xl lg:max-w-lg">
+            <h2 className="font-semibold text-4xl text-gray-900 text-pretty sm:text-5xl tracking-tight">
               Let's work together
             </h2>
-            <p className="mt-2 text-lg/8 text-gray-600">
+            <p className="mt-2 text-gray-600 text-lg/8">
               Proin volutpat consequat porttitor cras nullam gravida at orci
               molestie a eu arcu sed ut tincidunt magna.
             </p>
             <form action="#" method="POST" className="mt-16">
-              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div className="gap-x-8 gap-y-6 grid grid-cols-1 sm:grid-cols-2">
                 <div>
                   <label
                     htmlFor="first-name"
-                    className="block text-sm/6 font-semibold text-gray-900"
+                    className="block font-semibold text-gray-900 text-sm/6"
                   >
                     First name
                   </label>
@@ -35,14 +35,14 @@ export default function RequestUs() {
                       name="first-name"
                       type="text"
                       autoComplete="given-name"
-                      className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                      className="block bg-white px-3.5 py-2 rounded-md w-full text-base text-gray-900 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                     />
                   </div>
                 </div>
                 <div>
                   <label
                     htmlFor="last-name"
-                    className="block text-sm/6 font-semibold text-gray-900"
+                    className="block font-semibold text-gray-900 text-sm/6"
                   >
                     Last name
                   </label>
@@ -52,14 +52,14 @@ export default function RequestUs() {
                       name="last-name"
                       type="text"
                       autoComplete="family-name"
-                      className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                      className="block bg-white px-3.5 py-2 rounded-md w-full text-base text-gray-900 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                     />
                   </div>
                 </div>
                 <div className="sm:col-span-2">
                   <label
                     htmlFor="email"
-                    className="block text-sm/6 font-semibold text-gray-900"
+                    className="block font-semibold text-gray-900 text-sm/6"
                   >
                     Email
                   </label>
@@ -69,14 +69,14 @@ export default function RequestUs() {
                       name="email"
                       type="email"
                       autoComplete="email"
-                      className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                      className="block bg-white px-3.5 py-2 rounded-md w-full text-base text-gray-900 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                     />
                   </div>
                 </div>
                 <div className="sm:col-span-2">
                   <label
                     htmlFor="company"
-                    className="block text-sm/6 font-semibold text-gray-900"
+                    className="block font-semibold text-gray-900 text-sm/6"
                   >
                     Company
                   </label>
@@ -86,7 +86,7 @@ export default function RequestUs() {
                       name="company"
                       type="text"
                       autoComplete="organization"
-                      className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                      className="block bg-white px-3.5 py-2 rounded-md w-full text-base text-gray-900 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                     />
                   </div>
                 </div>
@@ -109,7 +109,7 @@ export default function RequestUs() {
                       type="tel"
                       autoComplete="tel"
                       aria-describedby="phone-description"
-                      className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                      className="block bg-white px-3.5 py-2 rounded-md w-full text-base text-gray-900 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                     />
                   </div>
                 </div>
@@ -117,7 +117,7 @@ export default function RequestUs() {
                   <div className="flex justify-between text-sm/6">
                     <label
                       htmlFor="message"
-                      className="block text-sm/6 font-semibold text-gray-900"
+                      className="block font-semibold text-gray-900 text-sm/6"
                     >
                       How can we help you?
                     </label>
@@ -131,18 +131,18 @@ export default function RequestUs() {
                       name="message"
                       rows={4}
                       aria-describedby="message-description"
-                      className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                      className="block bg-white px-3.5 py-2 rounded-md w-full text-base text-gray-900 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                       defaultValue={""}
                     />
                   </div>
                 </div>
-                <dl className="mt-10 space-y-4 text-base/7 text-gray-600">
+                <dl className="space-y-4 mt-10 text-base/7 text-gray-600">
                   <div className="flex gap-x-4">
                     <dt className="flex-none">
                       <span className="sr-only">Address</span>
                       <FaBuilding
                         aria-hidden="true"
-                        className="h-7 w-6 text-gray-400"
+                        className="w-6 h-7 text-gray-400"
                       />
                     </dt>
                     <dd>
@@ -156,7 +156,7 @@ export default function RequestUs() {
                       <span className="sr-only">Telephone</span>
                       <FaPhoneAlt
                         aria-hidden="true"
-                        className="h-7 w-6 text-gray-400"
+                        className="w-6 h-7 text-gray-400"
                       />
                     </dt>
                     <dd>
@@ -173,7 +173,7 @@ export default function RequestUs() {
                       <span className="sr-only">Email</span>
                       <FaEnvelope
                         aria-hidden="true"
-                        className="h-7 w-6 text-gray-400"
+                        className="w-6 h-7 text-gray-400"
                       />
                     </dt>
                     <dd>
@@ -187,10 +187,10 @@ export default function RequestUs() {
                   </div>
                 </dl>
               </div>
-              <div className="mt-10 flex justify-end border-t border-gray-900/10 pt-8">
+              <div className="flex justify-end mt-10 pt-8 border-gray-900/10 border-t">
                 <button
                   type="submit"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="bg-indigo-600 hover:bg-indigo-500 shadow-xs px-3.5 py-2.5 rounded-md font-semibold text-center text-sm text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Send message
                 </button>
