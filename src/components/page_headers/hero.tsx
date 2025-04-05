c29c4492caa511b5755f5c3f885227511074f05b
Tina Huynh <tinamchuynh@gmail.com>
2025-04-04 23:17:06 -0700

    Add HeroSection component for displaying a featured image with title case alt text

---
+++ src/components/page_headers/hero.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from "react";
+
+interface HeroProps {
+  imageSrc: string;
+}
+
+function titleCase(s: string) {
+  return s
+    .toLowerCase()
+    .split(" ")
+    .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+const HeroSection: FC<HeroProps> = ({ imageSrc }) => {
+  const imageArray = imageSrc.split("/");
+  const imageTitle = imageArray[imageArray.length - 1].split(".");
+
+  return (
+    <section className="relative overflow-hidden">
+      <div className="relative h-[30em] w-full">
+        <img
+          className="object-cover object-center w-full h-full"
+          src={imageSrc}
+          alt={titleCase(imageTitle[0]) + " Image"}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
