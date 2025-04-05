176100fd5a5f07aebf24eff212f4c4df2022afd2
Tina Huynh <tinamchuynh@gmail.com>
2025-04-05 01:31:14 -0700

    Refactor LogoCloud component to use a grid layout for improved responsiveness and structure

--- src/components/LogoCloud.tsx
+++ src/components/LogoCloud.tsx
@@ -2,14 +2,15 @@ import { logos } from "@/lib/constants/list";
 
 export default function LogoCloud() {
   return (
-    <div className="mx-auto flex flex-wrap gap-5 justify-center items-center">
+    <div className="mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-center items-center">
       {logos.map((logo, index) => (
-        <img
-          key={index}
-          alt={logo.alt}
-          src={logo.src}
-          className="object-cover h-24 md:h-36"
-        />
+        <div key={index} className="text-center">
+          <img
+            alt={logo.alt}
+            src={logo.src}
+            className="object-cover h-24 md:h-36 mx-auto"
+          />
+        </div>
       ))}
     </div>
   );
