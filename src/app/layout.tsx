b6e36f813a3d23ffec74c4a2df4d166e092cacb1
Tina Huynh <tinamchuynh@gmail.com>
2025-04-04 21:21:47 -0700

    Refactor layout imports: update BackToTop and StaticBreadcrumb paths for improved organization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import BackToTop from "@/components/BackToTop";
-import StaticBreadcrumb from "@/components/BreadcrumbWrapper";
 import { ThemeProvider } from "next-themes";
 import { NotFoundProvider } from "./context/NotFoundContext";
 import "./globals.css";
 import { Providers } from "./providers";
+import BackToTop from "@/components/buttons/BackToTop";
+import StaticBreadcrumb from "@/components/breadcrumb/BreadcrumbWrapper";
 
 export default function RootLayout({
   children,
