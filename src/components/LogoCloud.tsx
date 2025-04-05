146467ae14065b04709e91c2409e008d3c7a1220
Tina Huynh <tinamchuynh@gmail.com>
2025-04-05 02:15:40 -0700

    Refactor LogoCloud component to use Next.js Image for optimized image loading

--- src/components/LogoCloud.tsx
+++ src/components/LogoCloud.tsx
@@ -1,14 +1,17 @@
+import Image from "next/image";
 import { logos } from "@/lib/constants/list";
 
 export default function LogoCloud() {
   return (
-    <div className="mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-center items-center">
+    <div className="justify-center items-center gap-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-auto">
       {logos.map((logo, index) => (
         <div key={index} className="text-center">
-          <img
+          <Image
             alt={logo.alt}
             src={logo.src}
-            className="object-cover h-24 md:h-36 mx-auto"
+            width={144}
+            height={144}
+            className="mx-auto h-24 md:h-36 object-cover"
           />
         </div>
       ))}
