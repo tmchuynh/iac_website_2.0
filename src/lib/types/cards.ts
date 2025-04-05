45de93e5e92b3e86039ae786720550bafe314131
Tina Huynh <tinamchuynh@gmail.com>
2025-04-04 22:41:29 -0700

    Add type definitions for Person and Program in cards.ts

---
+++ src/lib/types/cards.ts
@@ -0,0 +1,12 @@
+export type Person = {
+  name: string;
+  role: string;
+  image: string;
+  description: string;
+};
+
+export type Program = {
+  title: string;
+  description: string;
+  image: string;
+};
