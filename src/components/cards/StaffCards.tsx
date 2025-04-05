5e3b499e9e9d0987a96569d0e1b646d1ce8205bd
Tina Huynh <tinamchuynh@gmail.com>
2025-04-04 22:40:49 -0700

    Enhance StaffCards component to display staff member details with image, name, role, and description

--- src/components/cards/StaffCards.tsx
+++ src/components/cards/StaffCards.tsx
@@ -1,3 +1,18 @@
-export const StaffCards = () => {
-  return <></>;
+import { Person } from "@/lib/types/cards";
+
+export const StaffCards = (person: Person) => {
+  return (
+    <div>
+      <img
+        alt=""
+        src={person.image}
+        className="aspect-3/2 w-full rounded-2xl object-cover"
+      />
+      <h3 className="mt-6 text-lg font-semibold tracking-tight">
+        {person.name}
+      </h3>
+      <p className="text-md">{person.role}</p>
+      {person.description && <p className="text-sm">{person.description}</p>}
+    </div>
+  );
 };
