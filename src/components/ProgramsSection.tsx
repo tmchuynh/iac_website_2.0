68f73c8ad840b0e7ec8672c5d638f93a9425dde1
Tina Huynh <tinamchuynh@gmail.com>
2025-04-05 22:41:42 -0700

    Increase gap between program cards in ProgramsSection for improved layout

--- src/components/ProgramsSection.tsx
+++ src/components/ProgramsSection.tsx
@@ -5,7 +5,7 @@ import { ProgramCards } from "./cards/ProgramCards";
 
 export default function ProgramsSection() {
   return (
-    <section className="gap-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto mb-12 px-6 w-11/12">
+    <section className="gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto mb-12 px-6 w-11/12">
       {programs.map((program, index) => (
         <ProgramCards program={program} index={index} key={program.title} />
       ))}
