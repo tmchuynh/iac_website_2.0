3221a7405ae374ac8a33b996ef3025aa88b5ceaf
Tina Huynh <tinamchuynh@gmail.com>
2025-04-06 01:03:35 -0700

    Refactor CannotFind component to use Next.js router for navigation

--- src/components/loading/CannotFind.tsx
+++ src/components/loading/CannotFind.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const CannotFind: React.FC = () => {
+  const router = useRouter();
   return (
     <main className="flex flex-col justify-center items-center mx-auto -mt-28 w-10/12 md:w-11/12 h-svh">
       <h1 className="mb-4 font-bold text-4xl">Page Not Found</h1>
@@ -8,7 +10,7 @@ const CannotFind: React.FC = () => {
         Sorry, the page you are looking for does not exist.
       </p>
       <button
-        onClick={() => navigator("/")}
+        onClick={() => router.push("/")}
         className="bg-blue-500 hover:bg-blue-600 px-6 py-3 rounded text-white"
       >
         Go Back to Homepage
