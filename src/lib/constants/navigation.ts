e0b292218a08365f02a76f639134f4c0a2875c57
Tina Huynh <tinamchuynh@gmail.com>
2025-04-05 21:53:30 -0700

    Add descriptions to navigation items for improved clarity

--- src/lib/constants/navigation.ts
+++ src/lib/constants/navigation.ts
@@ -11,40 +11,70 @@ import { HiMiniInboxArrowDown } from "react-icons/hi2";
 import { MdMarkEmailRead } from "react-icons/md";
 
 export const about = [
-  { name: "Our Story", href: "/about_us", icon: FaBook },
-  { name: "Our Commitment", href: "/about_us/our_commitment", icon: FaFlag },
-  { name: "The Mission", href: "/about_us/our_mission", icon: FaTrophy },
+  {
+    name: "Our Story",
+    href: "/about_us",
+    description: "Learn about the history and foundation of our program.",
+    icon: FaBook,
+  },
+  {
+    name: "Our Commitment",
+    href: "/about_us/our_commitment",
+    description: "Discover our dedication to providing high-quality education.",
+    icon: FaFlag,
+  },
+  {
+    name: "The Mission",
+    href: "/about_us/our_mission",
+    description: "Understand the core purpose and values driving our work.",
+    icon: FaTrophy,
+  },
 ];
 
 export const programs = [
-  { name: "Program Overview", href: "/programs", icon: FaFileAlt },
+  {
+    name: "Program Overview",
+    href: "/programs",
+    description: "Explore the programs we offer and how they benefit students.",
+    icon: FaFileAlt,
+  },
   {
     name: "Partnering Schools",
     href: "/programs/school_information",
+    description:
+      "Find out about the schools we partner with to deliver our programs.",
     icon: FaSchool,
   },
 ];
 
 export const contact = [
-  { name: "Contact Us", href: "/contact_us", icon: MdMarkEmailRead },
+  {
+    name: "Contact Us",
+    href: "/contact_us",
+    description: "Get in touch with us for any questions or information.",
+    icon: MdMarkEmailRead,
+  },
   {
     name: "Request IAC at Your School",
     href: "/contact_us/request_us",
+    description: "Learn how to bring our programs to your school.",
     icon: HiMiniInboxArrowDown,
   },
   {
     name: "Register For A Class",
     href: "/contact_us/register",
+    description: "Sign up for classes and start your learning journey.",
     icon: FaPencil,
   },
   {
     name: "Apply to Work With Us",
     href: "/contact_us/apply",
+    description: "Join our team and make an impact on students' lives.",
     icon: FaChalkboardTeacher,
   },
 ];
 
-export const recentPosts = [
+export const featuredLinks = [
   {
     id: 1,
     title: "The Staff Behind The Curtains",
