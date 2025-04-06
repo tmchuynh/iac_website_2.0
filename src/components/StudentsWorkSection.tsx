85f25b8a373811240d8e5c947187feefbd96d552
Tina Huynh <tinamchuynh@gmail.com>
2025-04-05 18:59:12 -0700

    Refactor StudentsWorkSection and StudentWorkCard for improved layout and styling

--- src/components/StudentsWorkSection.tsx
+++ src/components/StudentsWorkSection.tsx
@@ -14,7 +14,7 @@ export default function StudentsWorkSection() {
       </TabsList>
       {showcase.map((subject, index) => (
         <TabsContent value={subject.subject} key={index}>
-          <div className="flex flex-col gap-4">
+          <div className="gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {subject.works.map((work, index) => (
               <StudentWorkCard work={work} key={index} index={index} />
             ))}
--- src/components/cards/StudentWorkCard.tsx
+++ src/components/cards/StudentWorkCard.tsx
@@ -1,7 +1,7 @@
 import { Work } from "@/lib/types/information";
-import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 export default function StudentWorkCard({
   work,
@@ -20,46 +20,50 @@ export default function StudentWorkCard({
         work.date
       )}`}
     >
-      <Card
+      <div
         key={index}
-        className={index % 2 === 0 ? "border-r-8" : "border-l-8"}
+        className="group relative shadow-lg hover:shadow-xl rounded-4xl transition-all duration-500"
       >
-        <CardHeader className="">
-          <h4 className="text-lg tracking-widest">{work.title}</h4>
-          <p className="text-xs meta">
-            <span className="font-bold">By: </span>
-            {work.author}
-          </p>
-          <p className="text-xs meta">
-            <span className="font-bold">Date: </span>
-            {work.date}
-          </p>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-3">
-          {work.description}
-          <hr />
-          {work.preview && <p className="text-sm italic">{work.preview}...</p>}
-
-          {work.preview && work.images && <hr />}
-
-          {work.images && (
-            <div className="gap-4 grid grid-cols-3">
-              {work.images.map((image, index) => (
-                <Image
-                  key={index}
-                  src={image}
-                  alt={`${work.title} image ${index + 1}`}
-                  width={300}
-                  height={200}
-                  className="border border-input rounded-lg"
-                />
-              ))}
+        <div className="absolute inset-0 bg-gradient-to-tl from-secondary via-primary to-tertiary opacity-0 group-hover:opacity-100 rounded-2xl transition-opacity duration-500" />
+        <div className="relative bg-background m-[4px] rounded-2xl overflow-hidden">
+          <div className="relative">
+            <Image
+              alt={`${work.title}_${work.author}_${work.school}`}
+              className="w-full transform transition-transform duration-700 object-cover ease-out md:group-hover:scale-105 lg:group-hover:scale-110"
+              src={`${work.mainImage}`}
+              width={800}
+              height={600}
+            />
+          </div>
+          <div className="p-6">
+            <h3 className="mb-2 font-bold text-xl">{work.title}</h3>
+            <p className="mb-4 text-sm">{work.grade}</p>
+            <div className="flex justify-between items-center">
+              <span className="bg-clip-text bg-gradient-to-r from-secondary via-primary/90 to-tertiary/75 font-bold text-2xl text-transparent">
+                {work.school}
+              </span>
+              <Button className="group-hover:scale-105">
+                <svg
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-sparkles"
+                  fill="none"
+                  height={24}
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  viewBox="0 0 24 24"
+                  width={24}
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+                  <path d="M16 18a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm0 -12a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm-7 12a6 6 0 0 1 6 -6a6 6 0 0 1 -6 -6a6 6 0 0 1 -6 6a6 6 0 0 1 6 6z" />
+                </svg>
+                View Project
+              </Button>
             </div>
-          )}
-        </CardContent>
-
-        <CardFooter className="flex justify-end"></CardFooter>
-      </Card>
+          </div>
+        </div>
+      </div>
     </Link>
   );
 }
