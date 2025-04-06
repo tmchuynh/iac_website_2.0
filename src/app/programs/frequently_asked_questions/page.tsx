e78707bb145e863f87383a520111ad6b4be600d6
Tina Huynh <tinamchuynh@gmail.com>
2025-04-06 02:13:59 -0700

    Refactor Frequently Asked Questions page for improved layout and navigation, integrating dynamic button for contact support

--- src/app/programs/frequently_asked_questions/page.tsx
+++ src/app/programs/frequently_asked_questions/page.tsx
@@ -1,37 +1,41 @@
+"use client";
+
 import DynamicButton from "@/components/buttons/button-dynamic";
+import { faqs } from "@/lib/constants/frequently_asked_questions";
+import { useRouter } from "next/navigation";
 
-const faqs = [
-  {
-    question: "How do you make holy water?",
-    answer:
-      "You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
-  },
-];
-
-export default function FrequentlyAskedQuestions() {
+export default function Example() {
+  const router = useRouter();
   return (
-    <div className="">
-      <div className="mx-auto px-6 lg:px-8 py-24 lg:py-40 sm:pt-32 max-w-7xl">
-        <div className="lg:gap-8 lg:grid lg:grid-cols-12">
-          <div className="lg:col-span-5">
-            <h2 className="font-semibold text-3xl text-pretty sm:text-4xl tracking-tight">
-              Frequently asked questions
-            </h2>
-            <p className="mt-4 text-pretty">
-              Can’t find the answer you’re looking for?
-            </p>
-            <DynamicButton text="Contact Us" />
-          </div>
-          <div className="lg:col-span-7 mt-10 lg:mt-0">
-            <dl className="space-y-10">
-              {faqs.map((faq) => (
-                <div key={faq.question}>
-                  <dt className="font-semibold text-base/7">{faq.question}</dt>
-                  <dd className="mt-2">{faq.answer}</dd>
-                </div>
-              ))}
-            </dl>
-          </div>
+    <div>
+      <div className="mx-auto px-6 lg:px-8 py-16 sm:py-24 max-w-7xl">
+        <h2 className="font-semibold text-4xl sm:text-5xl tracking-tight">
+          Frequently asked questions
+        </h2>
+        <p className="mt-6 max-w-2xl">
+          Have a different question and can’t find the answer you’re looking
+          for? Reach out to our support team by{" "}
+          <DynamicButton
+            variant="link"
+            onClick={() => {
+              router.push("/contact_us");
+            }}
+          >
+            sending us an email{" "}
+          </DynamicButton>
+          and we’ll get back to you as soon as we can.
+        </p>
+        <div className="mt-20">
+          <dl className="sm:gap-x-6 sm:gap-y-16 lg:gap-x-10 space-y-16 sm:space-y-0 sm:grid sm:grid-cols-2 lg:grid-cols-3">
+            {faqs.map((faq, index) => (
+              <div key={index}>
+                <p className="border-b font-bold tracking-wider">
+                  {faq.question}
+                </p>
+                <p>{faq.answer}</p>
+              </div>
+            ))}
+          </dl>
         </div>
       </div>
     </div>
