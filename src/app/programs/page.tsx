667843d0b4924fa710e2e2ca1c02add1d3f35ce1
Tina Huynh <tinamchuynh@gmail.com>
2025-04-06 11:32:35 -0700

    Refactor ProgramsPage layout for improved structure and clarity

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -31,38 +31,45 @@ export default function ProgramsPage(): JSX.Element {
 
   return (
     <div className="flex flex-col justify-center items-center mx-auto px-4 py-20 w-11/12">
-      <h1 className="my-4 font-extrabold text-balance text-center text-lg text-secondary lg:text-4xl uppercase tracking-wider">
-        Our Programs
-      </h1>
-      <p>
-        To ensure an organized and impactful learning experience, our classes
-        are carefully designed with a structured approach that breaks down the
-        curriculum into several distinct segments. Each segment is strategically
-        crafted to focus on key aspects of learning, allowing students to fully
-        engage with the material and make the most of every lesson. This
-        segmented structure maximizes educational benefits by targeting specific
-        skills and knowledge areas at the right time, enhancing the overall
-        learning process.
-      </p>
-      <p>
-        By dividing the class into well-defined parts, we ensure that students
-        receive clear, focused instruction while also allowing for flexibility
-        and interactive learning. This structure promotes continuous engagement,
-        fosters a deeper understanding of the subject matter, and helps students
-        retain information more effectively. Whether it's introducing new
-        concepts, reinforcing previously learned material, or encouraging
-        collaborative activities, every segment serves a unique purpose to
-        create a comprehensive and enriching educational journey.
-      </p>
-      <DoughnutChartLabel />
-      <ul className="flex flex-col gap-y-3 py-9">
-        {class_breakdown.map((list, index) => (
-          <li key={index}>
-            <strong>{list.title}</strong> {list.description}
-          </li>
-        ))}
-      </ul>
-      <ProgramDetails title={title} />
+      <section>
+        <h1 className="my-4 font-extrabold text-balance text-center text-lg text-secondary lg:text-4xl uppercase tracking-wider">
+          Class Breakdown
+        </h1>
+        <p>
+          To ensure an organized and impactful learning experience, our classes
+          are carefully designed with a structured approach that breaks down the
+          curriculum into several distinct segments. Each segment is
+          strategically crafted to focus on key aspects of learning, allowing
+          students to fully engage with the material and make the most of every
+          lesson. This segmented structure maximizes educational benefits by
+          targeting specific skills and knowledge areas at the right time,
+          enhancing the overall learning process.
+        </p>
+        <p>
+          By dividing the class into well-defined parts, we ensure that students
+          receive clear, focused instruction while also allowing for flexibility
+          and interactive learning. This structure promotes continuous
+          engagement, fosters a deeper understanding of the subject matter, and
+          helps students retain information more effectively. Whether it's
+          introducing new concepts, reinforcing previously learned material, or
+          encouraging collaborative activities, every segment serves a unique
+          purpose to create a comprehensive and enriching educational journey.
+        </p>
+        <DoughnutChartLabel />
+        <ul className="flex flex-col gap-y-3 py-9">
+          {class_breakdown.map((list, index) => (
+            <li key={index}>
+              <strong>{list.title}</strong> {list.description}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h1 className="my-4 font-extrabold text-balance text-center text-lg text-secondary lg:text-4xl uppercase tracking-wider">
+          Our Program Details
+        </h1>
+        <ProgramDetails title={title} />
+      </section>
     </div>
   );
 }
