d477812d9db864be646c951c90ab9513b465e988
Tina Huynh <tinamchuynh@gmail.com>
2025-04-05 22:41:47 -0700

    Update ContentAndScreenshot component with enhanced text and dynamic button for improved user engagement

--- src/components/page_headers/ContentAndScreenshot.tsx
+++ src/components/page_headers/ContentAndScreenshot.tsx
@@ -1,55 +1,66 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 import Image from "next/image";
+import { Button } from "../ui/button";
+import DynamicButton from "../buttons/button-dynamic";
 
 export default function ContentAndScreenshot() {
+  const router = useRouter();
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="py-24 sm:py-32">
       <div className="mx-auto px-6 lg:px-8 max-w-7xl">
         <div className="mx-auto lg:mx-0 max-w-2xl lg:max-w-none">
-          <p className="font-semibold text-base/7 text-indigo-600">
-            Deploy faster
+          <div className="md:max-w-lg">
+            <Image
+              alt=""
+              src="/images/purple_logo.png"
+              width={3600}
+              height={1200}
+            />
+          </div>
+          <p className="font-semibold text-secondary uppercase">
+            Bringing Enrichment to Your School
           </p>
-          <h1 className="mt-2 font-semibold text-4xl text-gray-900 text-pretty sm:text-5xl tracking-tight">
-            A better workflow
+          <h1 className="mt-2 font-semibold text-4xl text-pretty sm:text-5xl tracking-tight">
+            Superior classes, exceptional instructors, outstanding outcomes.
           </h1>
-          <div className="gap-8 grid grid-cols-1 lg:grid-cols-2 mt-10 max-w-xl lg:max-w-none text-base/7 text-gray-700">
+          <div className="gap-8 grid grid-cols-1 lg:grid-cols-2 mt-10 max-w-xl lg:max-w-none">
             <div>
               <p>
-                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-                risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-                Id dolor praesent donec est. Odio penatibus risus viverra tellus
-                varius sit neque erat velit. Faucibus commodo massa rhoncus,
-                volutpat. Dignissim sed eget risus enim. Mattis mauris semper
-                sed amet vitae sed turpis id.
-              </p>
-              <p className="mt-8">
-                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
-                odio id et. Id blandit molestie auctor fermentum dignissim.
-                Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate
-                et ultrices hac adipiscing egestas.
+                Our dynamic after-school program offers an exciting range of
+                activities, including soccer, volleyball, basketball, performing
+                arts, creative writing, chess, and coding. With experienced
+                instructors and top-quality resources, we provide a fun and
+                enriching environment where your child can explore and develop
+                new passions. Our innovative approach covers a broad spectrum of
+                skills in sports, arts, and technology. Your child will enjoy
+                engaging in creative projects, teamwork challenges, and much
+                more, all within a safe, supportive setting tailored for their
+                growth and success.
               </p>
             </div>
             <div>
               <p>
-                Erat pellentesque dictumst ligula porttitor risus eget et eget.
-                Ultricies tellus felis id dignissim eget. Est augue maecenas
-                risus nulla ultrices congue nunc tortor.
+                Our programs have earned widespread popularity and strong
+                approval from both students and parents alike.
               </p>
               <p className="mt-8">
-                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
-                odio id et. Id blandit molestie auctor fermentum dignissim.
-                Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate
-                et ultrices hac adipiscing egestas. Iaculis convallis ac tempor
-                et ut. Ac lorem vel integer orci.
+                Our programs consistently uphold high standards of quality and
+                excellence across all activities.
+              </p>
+              <p className="mt-8">
+                IAC is committed to making our programs accessible and
+                affordable for all!
               </p>
             </div>
           </div>
           <div className="flex mt-10">
-            <a
-              href="#"
-              className="bg-indigo-600 hover:bg-indigo-500 shadow-xs px-3.5 py-2.5 rounded-md font-semibold text-sm text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Get started
-            </a>
+            <DynamicButton
+              text="Get Started Today"
+              className="w-1/4"
+              onClick={() => router.push("/")}
+            />
           </div>
         </div>
       </div>
@@ -57,13 +68,13 @@ export default function ContentAndScreenshot() {
         <div className="mx-auto px-6 lg:px-8 max-w-7xl">
           <Image
             alt=""
-            src="https://tailwindcss.com/plus-assets/img/component-images/project-app-screenshot.png"
-            className="shadow-2xl mb-[-12%] rounded-xl ring-1 ring-gray-900/10"
+            src="/images/Class_Photos/Soccer/53995092_144851.jpg"
+            className="shadow-2xl mb-[-12%] rounded-xl ring-1 ring-gray-900/10 aspect-video object-cover"
             width={3600}
             height={1200}
           />
           <div aria-hidden="true" className="relative">
-            <div className="bottom-0 absolute -inset-x-20 bg-linear-to-t from-white pt-[7%]" />
+            <div className="bottom-0 absolute -inset-x-20 bg-linear-to-t from-white pt-[27%]" />
           </div>
         </div>
       </div>
