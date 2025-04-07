983ccaaeaa1d2358a076e97f28b79d9440e067f7
Tina Huynh <tinamchuynh@gmail.com>
2025-04-06 21:04:40 -0700

    Refactors testimonials page layout and adds image overlay
    Updates grid layout to support multiple columns
    Adds image overlay with satisfaction icon to each testimonial
    Increases size of testimonial images

--- src/app/programs/testimonials/page.tsx
+++ src/app/programs/testimonials/page.tsx
@@ -8,27 +8,30 @@ export default function TestimonialsPage() {
       <h1 className="pb-4 max-w-4xl font-extrabold text-balance text-primary text-xl md:text-2xl lg:text-4xl xl:text-5xl uppercase tracking-wider">
         We have worked with thousands of amazing people
       </h1>
-      <div className="gap-4 grid grid-cols-1 md:grid-cols-2">
+      <div className="gap-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
         {testimonials.map((testimonial, index) => (
           <figure
             key={index}
-            className="shadow-lg p-6 rounded-2xl ring-1 ring-gray-900/5"
+            className="relative flex flex-col justify-between shadow-lg p-6 rounded-2xl ring-1 ring-gray-900/5 overflow-clip"
           >
+            <Image
+              alt=""
+              src="/images/icons/satisfaction.png"
+              className="top-[50%] left-[50%] -z-10 absolute opacity-5 w-1/2 h-1/2 -translate-x-1/2 -translate-y-1/2 object-contain"
+              width={500}
+              height={500}
+            />
             <blockquote className="text-gray-900">
               <p>{`“${testimonial.text}”`}</p>
             </blockquote>
-            <figcaption className="flex items-center gap-x-4 mt-6">
+            <figcaption className="flex justify-end items-center gap-x-4 mt-6">
               <Image
                 alt=""
                 src={testimonial.image}
-                className="rounded-full size-10"
-                width={40}
-                height={40}
+                className="z-10 rounded-full"
+                width={100}
+                height={100}
               />
-              <div>
-                <div className="font-semibold">{testimonial.title}</div>
-                <div className="">{testimonial.school}</div>
-              </div>
             </figcaption>
           </figure>
         ))}
