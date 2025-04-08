8c1e5b3e2718f181808e5f66e229d46f4e02c6d4
Tina Huynh <tinamchuynh@gmail.com>
2025-04-07 21:06:12 -0700

    Updates StaffCards component for dark mode support
    Reorders imports and removes unused import.
    Improves icon color handling to support dark mode.
    Fixes group-hover class removal for header text.

--- src/components/cards/StaffCards.tsx
+++ src/components/cards/StaffCards.tsx
@@ -1,10 +1,10 @@
 import { Person } from "@/lib/types/cards";
 import { formatIconName, formatURL } from "@/lib/utils/format";
+import { TooltipTrigger } from "@radix-ui/react-tooltip";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import DynamicButton from "../buttons/button-dynamic";
 import { Tooltip, TooltipContent } from "../ui/tooltip";
-import { TooltipTrigger } from "@radix-ui/react-tooltip";
 
 export const StaffCards = (person: Person) => {
   const router = useRouter();
@@ -30,7 +30,7 @@ export const StaffCards = (person: Person) => {
       <div className="flex flex-col justify-between p-6 h-full">
         <section className="flex flex-col justify-between gap-4 h-full">
           <div>
-            <h3 className="font-semibold text-xl group-hover:text-gray-900 tracking-tight">
+            <h3 className="font-semibold text-xl tracking-tight">
               {person.name}
             </h3>
             <p className="text-sm">{person.role}</p>
@@ -40,7 +40,7 @@ export const StaffCards = (person: Person) => {
               {person.icons.map((Icon, index) => (
                 <Tooltip key={index}>
                   <TooltipTrigger>
-                    <Icon className="w-6 h-6 text-primary transition-colors" />
+                    <Icon className="w-6 h-6 text-primary dark:text-chart-2 transition-colors" />
                   </TooltipTrigger>
                   <TooltipContent>{formatIconName(Icon.name)}</TooltipContent>
                 </Tooltip>
