e4ced0f2450286362670896c9d0a64491e76d948
Tina Huynh <tinamchuynh@gmail.com>
2025-04-08 02:06:41 -0700

    Refactor ProvideFeedbackPage component to improve readability by removing unnecessary whitespace

--- src/app/contact_us/provide_feedback/page.tsx
+++ src/app/contact_us/provide_feedback/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import FeedbackForm from "@/components/forms/FeedbackForm";
 import FadedImage from "@/components/images/FadedImage";
 
@@ -10,7 +9,6 @@ export default function ProvideFeedbackPage() {
       <h1 className="my-4 pb-4 font-extrabold text-balance text-primary text-xl md:text-2xl lg:text-4xl xl:text-5xl dark:text-chart-2 uppercase tracking-wider">
         We Value Your Feedback
       </h1>
-
       <FadedImage imageSrc="/images/Class_Photos/Basketball/20156202_141123.JPG" />
       <p>
         At International Activities Club, we’re deeply committed to delivering
@@ -19,7 +17,6 @@ export default function ProvideFeedbackPage() {
         learning happens through intentional design, inclusive experiences, and
         continuous reflection. That’s why your feedback is so important to us.
       </p>
-
       <p>
         Whether you've participated in one of our enrichment classes, partnered
         with us as a school, or experienced our programming in your community,
@@ -28,16 +25,13 @@ export default function ProvideFeedbackPage() {
         curiosity, nurtures creativity, and builds lasting confidence in
         learners of all backgrounds.
       </p>
-
       <p>
         Together, with your input, we can continue to grow, evolve, and create
         experiences that truly make a difference.
       </p>
-
       <h2 className="my-4 font-extrabold text-balance text-lg text-secondary lg:text-4xl uppercase tracking-wider">
         How You Can Help
       </h2>
-
       <p>
         Your voice plays a vital role in shaping the future of our programs. By
         sharing your thoughts and experiences, you help us better understand
@@ -49,7 +43,6 @@ export default function ProvideFeedbackPage() {
         response—big or small—makes a difference. Here are a few ways you can
         contribute:
       </p>
-
       <ul className="flex flex-col gap-2 py-2 list-disc list-inside">
         <li>
           <strong>Share your recent experience:</strong> Tell us what stood out,
@@ -73,13 +66,11 @@ export default function ProvideFeedbackPage() {
           creative suggestions are all welcome.
         </li>
       </ul>
-
       <p>
         All feedback is reviewed with care, and you have the option to remain
         anonymous if you prefer. Your insights are truly invaluable to us, and
         we’re grateful for the opportunity to learn and grow with your support.
       </p>
-
       <FeedbackForm />
     </main>
   );
