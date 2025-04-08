af675e5c071a05bf603eb2920b2dc26361b2e9c1
Tina Huynh <tinamchuynh@gmail.com>
2025-04-07 23:12:04 -0700

    Refactor ProgramsPage layout to include ImageTilesHeader and streamline structure

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -1,13 +1,12 @@
 "use client";
-
 import { useTabs } from "@/app/context/TabsContext";
 import ApplyToJoinCTA from "@/components/CTA/ApplyToJoinCTA";
 import DoughnutChartLabel from "@/components/images/PieChart";
 import ResponsiveLogo from "@/components/images/ResponsiveLogo";
 import LoadingIndicator from "@/components/loading/Loading";
+import ImageTilesHeader from "@/components/page_headers/ImageTilesHeader";
 import ProgramDetails from "@/components/programs/ProgramInformation";
 import { class_breakdown } from "@/lib/constants/list";
-import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
@@ -25,67 +24,54 @@ function ProgramContent() {
 
 export default function ProgramsPage() {
   return (
-    <div className="flex flex-col justify-center items-center mx-auto px-4 py-10 lg:py-20 w-11/12">
-      <div className="xl:mx-auto xl:px-8 py-3 md:py-6 lg:py-9 2xl:py-20 xl:py-12 lg:max-w-7xl">
-        <Image
-          alt=""
-          src="/images/Class_Photos/Soccer/53995092_146958.jpg"
-          className="shadow-xl xl:rounded-3xl w-full aspect-5/2 object-cover"
-          width={2832}
-          height={1130}
-        />
+    <>
+      <ImageTilesHeader />
+      <div className="flex flex-col justify-center items-center mx-auto px-4 w-11/12">
+        <section>
+          <Suspense fallback={<LoadingIndicator />}>
+            <ProgramContent />
+          </Suspense>
+        </section>
+
+        <ResponsiveLogo />
+
+        <section>
+          <h1 className="my-4 pb-4 font-extrabold text-balance text-primary text-xl md:text-2xl lg:text-4xl xl:text-5xl dark:text-chart-2 uppercase tracking-wider">
+            Class Breakdown
+          </h1>
+          <p>
+            To ensure an organized and impactful learning experience, our
+            classes are carefully designed with a structured approach that
+            breaks down the curriculum into several distinct segments. Each
+            segment is strategically crafted to focus on key aspects of
+            learning, allowing students to fully engage with the material and
+            make the most of every lesson. This segmented structure maximizes
+            educational benefits by targeting specific skills and knowledge
+            areas at the right time, enhancing the overall learning process.
+          </p>
+          <DoughnutChartLabel />
+          <p>
+            By dividing the class into well-defined parts, we ensure that
+            students receive clear, focused instruction while also allowing for
+            flexibility and interactive learning. This structure promotes
+            continuous engagement, fosters a deeper understanding of the subject
+            matter, and helps students retain information more effectively.
+            Whether it's introducing new concepts, reinforcing previously
+            learned material, or encouraging collaborative activities, every
+            segment serves a unique purpose to create a comprehensive and
+            enriching educational journey.
+          </p>
+          <ul className="flex flex-col gap-y-3 py-4 list-disc">
+            {class_breakdown.map((list, index) => (
+              <li key={index}>
+                <strong>{list.title}</strong> {list.description}
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <ApplyToJoinCTA />
       </div>
-
-      <section>
-        <code className="font-semibold text-base/7">
-          Looking for a program that’s right for you?
-        </code>
-        <h1 className="my-4 pb-4 font-extrabold text-balance text-primary text-xl md:text-2xl lg:text-4xl xl:text-5xl dark:text-chart-2 uppercase tracking-wider">
-          Our Program Details
-        </h1>
-
-        <Suspense fallback={<LoadingIndicator />}>
-          <ProgramContent />
-        </Suspense>
-      </section>
-
-      <ResponsiveLogo />
-
-      <section>
-        <h1 className="my-4 pb-4 font-extrabold text-balance text-primary text-xl md:text-2xl lg:text-4xl xl:text-5xl dark:text-chart-2 uppercase tracking-wider">
-          Class Breakdown
-        </h1>
-        <p>
-          To ensure an organized and impactful learning experience, our classes
-          are carefully designed with a structured approach that breaks down the
-          curriculum into several distinct segments. Each segment is
-          strategically crafted to focus on key aspects of learning, allowing
-          students to fully engage with the material and make the most of every
-          lesson. This segmented structure maximizes educational benefits by
-          targeting specific skills and knowledge areas at the right time,
-          enhancing the overall learning process.
-        </p>
-        <DoughnutChartLabel />
-        <p>
-          By dividing the class into well-defined parts, we ensure that students
-          receive clear, focused instruction while also allowing for flexibility
-          and interactive learning. This structure promotes continuous
-          engagement, fosters a deeper understanding of the subject matter, and
-          helps students retain information more effectively. Whether it's
-          introducing new concepts, reinforcing previously learned material, or
-          encouraging collaborative activities, every segment serves a unique
-          purpose to create a comprehensive and enriching educational journey.
-        </p>
-        <ul className="flex flex-col gap-y-3 py-4 list-disc">
-          {class_breakdown.map((list, index) => (
-            <li key={index}>
-              <strong>{list.title}</strong> {list.description}
-            </li>
-          ))}
-        </ul>
-      </section>
-
-      <ApplyToJoinCTA />
-    </div>
+    </>
   );
 }
