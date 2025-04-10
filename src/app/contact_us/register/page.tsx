34793b3acbc8ab5b42e96326b23a818969d952cb
Tina Huynh <tinamchuynh@gmail.com>
2025-04-09 23:07:58 -0700

    Enhances registration page with dynamic navigation and content
    Adds dynamic buttons for navigation to contact and feedback pages
    Introduces detailed instructional and welcoming text for users
    Updates image and credit card processing procedure details
    
    Improves user experience and engagement with enhanced content

--- src/app/contact_us/register/page.tsx
+++ src/app/contact_us/register/page.tsx
@@ -1,12 +1,15 @@
 "use client";
+import DynamicButton from "@/components/buttons/button-dynamic";
 import RegisterForm from "@/components/forms/RegisterForm";
 import FadedImage from "@/components/images/FadedImage";
 import LoadingIndicator from "@/components/loading/Loading";
 import { credit_card_procedure } from "@/lib/constants/list";
+import { useRouter } from "next/navigation";
 import { FC, useState } from "react";
 
 const Register: FC = () => {
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
   if (loading) {
     <LoadingIndicator />;
@@ -21,7 +24,48 @@ const Register: FC = () => {
         Register Today
       </h1>
 
-      <FadedImage imageSrc="/images/Class_Photos/Basketball/20156202_141123.JPG" />
+      <p>
+        We are thrilled to have you here! Whether you’re a first time student or
+        returning student, we invite you to register for our programs and
+        experience the joy of learning and growth with us. Our classes are
+        designed to inspire, engage, and empower students of all ages. Join us
+        in creating unforgettable memories and meaningful connections. Let’s
+        embark on this exciting journey together!
+      </p>
+
+      <DynamicButton
+        onClick={() => router.push("/contact_us")}
+        className="md:hidden mb-3 w-full"
+      >
+        Contact Us
+      </DynamicButton>
+
+      <p>
+        To register for a program, please fill out the form below. If you have
+        any questions or need assistance, feel free to reach out to us. We’re
+        here to help you every step of the way. Thank you for choosing us as
+        your partner in learning and growth. We can’t wait to welcome you to our
+        community!
+      </p>
+      <p>
+        Please note that we have updated our credit card processing procedure.
+        To ensure a smooth and secure transaction, we kindly ask you to follow
+        the new steps outlined below. Your cooperation is greatly appreciated as
+        we strive to enhance your experience with us. Thank you for your
+        understanding and support!
+      </p>
+      <p>
+        We are excited to announce that we have partnered with a new payment
+        processor to enhance your registration experience. This change will
+        allow us to provide you with a more secure and efficient payment
+        process. As part of this transition, we have updated our credit card
+        processing procedure. We appreciate your understanding and support as we
+        work to improve our services. If you have any questions or concerns,
+        please feel free to reach out to us. Thank you for being a valued member
+        of our community!
+      </p>
+
+      <FadedImage imageSrc="/images/Class_Photos/Flag_Football/36252635_241856.jpg" />
 
       <h3 className="font-[Caveat] font-bold text-lg text-tertiary md:text-xl lg:text-2xl xl:text-3xl">
         New Credit Card Processing Procedure
@@ -33,6 +77,22 @@ const Register: FC = () => {
           </li>
         ))}
       </ol>
+
+      <p>
+        If you are a returning parent/student, we would also love to hear about
+        your experience in our programs and classes. Your feedback is invaluable
+        to us and helps us continue to improve and provide the best possible
+        experience for our students. Please take a moment to share your thoughts
+        with us by filling out the feedback form below. Thank you for your
+        continued support!
+      </p>
+
+      <DynamicButton
+        onClick={() => router.push("/contact_us/provide_feedback")}
+        className="md:hidden mb-12 w-full"
+      >
+        Provide Feedback
+      </DynamicButton>
 
       <RegisterForm />
     </main>
