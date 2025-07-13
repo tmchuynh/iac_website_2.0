c29cb10de175747890fa1091c18c60924e863b53
Tina Huynh <tinamchuynh@gmail.com>
2025-07-12 22:11:50 -0700

    Adds school request form and enhances user communication
    Introduces a new school request form component to replace the parent student request form, improving clarity for users.
    
    Enhances the user experience with informative messaging about the response process and immediate assistance options.
    
    Incorporates a dynamic button for direct phone calls, streamlining communication with the program.

--- src/app/programs/student_showcase/page.tsx
+++ src/app/programs/student_showcase/page.tsx
@@ -1,8 +1,10 @@
 "use client";
+import DynamicButton from "@/components/buttons/button-dynamic";
 import CenteredSimple from "@/components/CTA/CenteredSimple";
-import { RequestFormParentStudent } from "@/components/forms/RequestFormParentStudent";
+import RequestFormSchool from "@/components/forms/RequestFormSchool";
 import PageHeaderTestimonial from "@/components/page_headers/PageHeaderTestimonial";
 import StudentsWorkSection from "@/components/programs/StudentsWorkSection";
+import { useRouter } from "next/navigation";
 import { JSX } from "react";
 
 /**
@@ -19,6 +21,8 @@ import { JSX } from "react";
  * @returns {JSX.Element} A main container with multiple sections for the student showcase
  */
 export default function StudentShowcasePage(): JSX.Element {
+  const router = useRouter();
+
   return (
     <main className="mx-auto px-4 py-10 lg:py-20 w-11/12">
       <section className="mb-12">
@@ -116,7 +120,67 @@ export default function StudentShowcasePage(): JSX.Element {
       </section>
 
       <section className="mb-12">
-        <RequestFormParentStudent />
+        <h1 className="my-4 pb-4 font-[LuckiestGuy] font-extrabold text-balance text-primary text-xl md:text-2xl lg:text-4xl xl:text-5xl tracking-wider uppercase">
+          Request Us @ Your School
+        </h1>
+        <div className="-mb-11 mx-auto">
+          <div className="mb-4 leading-relaxed text-pretty">
+            <h2 className="font-[PermanentMarker] font-bold text-lg text-secondary md:text-xl lg:text-2xl">
+              We’re Excited to Connect With You!
+            </h2>
+            <p>
+              Thank you for reaching out to IAC’s A.R.C. Program! We truly
+              appreciate your interest in bringing our enriching,
+              student-centered programs to your school. Your message has been
+              received, and our team is reviewing it with care.
+            </p>
+            <p>
+              We value the time you've taken to connect with us, and we’re
+              committed to providing thoughtful, timely responses to all
+              inquiries. Whether you’re requesting more information, exploring
+              partnership options, or looking to schedule a consultation, we’re
+              here to help every step of the way.
+            </p>
+            <div className="my-5">
+              <h2 className="font-[PermanentMarker] font-bold text-lg text-secondary md:text-xl lg:text-2xl">
+                Here’s What to Expect:
+              </h2>
+              <ul className="flex flex-col gap-2 py-2 list-disc list-inside">
+                <li>
+                  A member of our team will get back to you as soon as possible
+                  with the information or support you need.
+                </li>
+                <li>
+                  In the meantime, feel free to explore more about our programs,
+                  curriculum offerings, or partnership benefits on our website.
+                </li>
+                <li>
+                  If your request is time-sensitive or urgent, we encourage you
+                  to reach out directly by phone.
+                </li>
+              </ul>
+            </div>
+            <div className="mt-5">
+              <h2 className="font-[PermanentMarker] font-bold text-lg text-secondary md:text-xl lg:text-2xl">
+                Need Immediate Assistance?
+              </h2>
+              <p>
+                Call our office at
+                <DynamicButton
+                  variant="link"
+                  onClick={() => router.push("tel:7145090069")}
+                >
+                  (714) 509-0069
+                </DynamicButton>
+                We look forward to the opportunity to collaborate and help
+                create meaningful, future-focused learning experiences for your
+                students. Thank you for considering IAC’s A.R.C. as your
+                educational partner!
+              </p>
+            </div>
+          </div>
+        </div>
+        <RequestFormSchool />
       </section>
     </main>
   );
