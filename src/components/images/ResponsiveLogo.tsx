5d40052d042d6aeb60e310b66b5e6c521db1e96f
Tina Huynh <tinamchuynh@gmail.com>
2025-07-13 18:44:51 -0700

    Refactors ResponsiveLogo component to remove theme handling and simplify image rendering

--- src/components/images/ResponsiveLogo.tsx
+++ src/components/images/ResponsiveLogo.tsx
@@ -1,42 +1,18 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 export default function ResponsiveLogo({ className }: { className?: string }) {
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
-
   return (
     <div className={`md:max-w-lg 2xl:max-w-none mx-auto ${className}`}>
-      {theme === "dark" ? (
-        <Image
-          alt="IAC Logo"
-          src="/images/white_logo.png"
-          className="h-40 xl:h-52 w-auto object-contain"
-          priority={true}
-          width={3600}
-          height={1200}
-        />
-      ) : (
-        <Image
-          alt="IAC Logo"
-          src="/images/purple_logo.png"
-          className="h-40 xl:h-52 w-auto object-contain"
-          priority={true}
-          width={3600}
-          height={1200}
-        />
-      )}
+      <Image
+        alt="IAC Logo"
+        src="/images/purple_logo.png"
+        className="h-40 xl:h-52 w-auto object-contain"
+        priority={true}
+        width={3600}
+        height={1200}
+      />
     </div>
   );
 }
